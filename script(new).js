b0bcc16ccfdd512d59434c2942c79c60dcc9ed5b
gloryade129 <adeniranglory129@gmail.com>
2024-08-21 11:49:02 +0100

    Add files via upload

---
+++ script(new).js
@@ -0,0 +1,9 @@
+document.getElementById('subscribe-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for subscribing!');
+});
+
+document.getElementById('comment-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your comment!');
+});
